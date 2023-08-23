804e579b24bbd7b89e4a82533a5f25bfe1fe520e
Melvin Quashie <melvinquashie@outlook.com>
2023-08-23 22:01:04 +0000

    update main.c for betty's sake

--- main.c
+++ main.c
@@ -7,29 +7,27 @@
  * @argv: arg vector list
  * Return: 0 on success or EXIT_FAILURE
  */
+#include "monty.h"
+
 int main(int argc, char *argv[])
 {
-	char opcode[6] = {0}, wrong[1024] = {0};
-	ssize_t read;
-	size_t len;
 	stack_t *stack = NULL;
 	int matches = 0;
 	unsigned int line_number = 0;
 
-	if (argc != 2)	/* if too many or too few arguments to monty exit fail */
+	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	globes.fm = NULL;
-	globes.fm = fopen(argv[1], "r"); /* not freed must close file */
-	if (globes.fm == NULL)	/* if file cant open then exit fail */
+
+	globes.fm = fopen(argv[1], "r");
+	if (globes.fm == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		fclose(globes.fm);
 		exit(EXIT_FAILURE);
 	}
-	globes.lineptr = NULL;
+
 	while ((read = getline(&globes.lineptr, &len, globes.fm)) != -1)
 	{
 		line_number++;
@@ -48,8 +46,9 @@ int main(int argc, char *argv[])
 		free(globes.lineptr);
 		globes.lineptr = NULL;
 	}
+
 	exit_free(stack);
-	return (0);
+	return (EXIT_SUCCESS);
 }
 
 /**
